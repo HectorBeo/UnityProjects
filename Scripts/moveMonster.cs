f4a5ba3b76ad4d99a81df2788120cb967e2d40b2
HectorBeo <hecordbeo@gmail.com>
2016-05-25 12:35:09 -0300

    new script patrol

---
+++ Scripts/moveMonster.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveMonster : MonoBehaviour {
+
+	public float speed;
+	private bool direction;
+	public float timeDirection;
+	
+	private float timeOnDirection = 0;
+	
+	void Update () {
+		if(direction)
+		{
+			transform.Translate(Vector2.left * speed * Time.deltaTime);
+		} else {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		}
+		
+		timeOnDirection += Time.deltaTime;
+		
+		if(timeOnDirection >= timeDirection)
+		{
+			timeOnDirection = 0;
+			direction = !direction;
+		}
+	}
+
+}
